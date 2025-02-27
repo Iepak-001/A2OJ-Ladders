337ec1d39f8a56016c3b49f9e4afc1733a9507bb
Deepak <deeepak2002@gmail.com>
2025-02-27 17:25:00 +0530

    Accepted

---
+++ hungrySequence.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i=1000000;
+    for(int j=0;j<n;j++){
+        cout<<i+j<<" ";
+    }
+    return 0;
+}
