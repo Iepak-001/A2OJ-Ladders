b67b55f2568c5c4583954f9336aacd6b69784212
Deepak <deeepak2002@gmail.com>
2025-03-02 10:38:25 +0530

    Accepted

---
+++ countingKangaroos.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    sort(arr.begin(),arr.end());
+
+    int lo=0;
+    int hi=(n+1)/2;
+
+    int count=0;
+    while(hi<n){
+        if(arr[hi]>=2*arr[lo]){
+            count++;
+            hi++;
+            lo++;
+        }
+        else hi++;
+    }
+    cout<<n-count;
+    return 0;
+}
