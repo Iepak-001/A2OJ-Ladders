65517839368773d4c0c07c20dafb2e17fd7c2a92
Deepak <deeepak2002@gmail.com>
2025-03-05 20:21:37 +0530

    Ceiling concept

---
+++ wonderRoom.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n,a,b;
+    cin>>n>>a>>b;
+
+
+    long long reqArea=6*n;
+
+    if(a*b >=reqArea){
+        cout<<a*b<<"\n";
+        cout<<a<<" "<<b;
+        return 0;
+    
+    }
+    long long tempa=a,tempb=b;
+
+    if(b<a) swap(a,b);
+
+    long long newB=b;
+    long long newA=a;
+
+    long long minA=a,minB=b,minArea=LLONG_MAX;
+    for(newA=a; newA*newA<=reqArea*2;newA++){
+        
+        newB=(reqArea+newA-1)/newA;
+        
+        if (newB < b) continue;
+
+        if(newB*newA<minArea && newA*newB>=reqArea){
+            minArea=newA*newB;
+            minA=newA;
+            minB=newB;
+            if(newA*newB==reqArea) break;
+        }
+    }
+    if(tempa>tempb) swap(minA,minB);
+    cout<<minA*minB<<"\n";
+    cout<<minA<<" "<<minB;
+    return 0;
+}
