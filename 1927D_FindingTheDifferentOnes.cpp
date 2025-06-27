a2e8007271f1fbd6b10858563fc6b1a6abd1140f
Deepak <deeepak2002@gmail.com>
2025-06-27 23:39:45 +0530

    updated

---
+++ 1927D_FindingTheDifferentOnes.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++) cin>>arr[i];
+
+        //Now do the processing
+        vector<int>positionOfNearestLeftDifferentElement(n);
+        positionOfNearestLeftDifferentElement[0]=-1;
+
+        for(int i=1;i<n;i++){
+            if(arr[i]==arr[i-1]) positionOfNearestLeftDifferentElement[i]=positionOfNearestLeftDifferentElement[i-1];
+            else positionOfNearestLeftDifferentElement[i]=i-1;
+        }
+
+        int q;
+        cin>>q;
+        while(q--){
+            int l,r;
+            cin>>l>>r;
+
+            l--,r--;
+
+            int rightPos=r;
+            int leftPos=positionOfNearestLeftDifferentElement[r];
+
+            if(leftPos<l){
+                cout<<("-1 -1");
+            }
+            else{
+                cout<<leftPos+1<<" "<<rightPos+1;
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
